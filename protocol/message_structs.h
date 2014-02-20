8e169c2c419d0b2c506fc99190e19231b94b5be2
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-02-20 13:09:50 -0800

    Move the constant data structures defined in protocol/message_structs.h to their own .c file, so as to make the linker happy

--- protocol/message_structs.h
+++ protocol/message_structs.h
@@ -70,17 +70,12 @@ typedef struct __attribute__((__packed__)){
 
 //typedef struct __attribute__((__packed__)){
 //} rsp_set_param_t;
-  
-  
-const uint8_t imc_message_type_count = 6;    // number of message types defined.
 
-const uint8_t imc_message_length[7] = {0, sizeof(msg_initialize_t), 
-                              0/*sizeof(msg_status_t)*/, 0/*sizeof(msg_home_t)*/, sizeof(msg_queue_move_t), 
-                              sizeof(msg_get_param_t), sizeof(msg_set_param_t)};
 
-const uint8_t imc_resp_length[7] = {0, sizeof(rsp_initialize_t), 
-                              sizeof(rsp_status_t), sizeof(rsp_home_t), 0/*sizeof(rsp_queue_move_t)*/,
-                              sizeof(rsp_get_param_t), 0/*sizeof(rsp_set_param_t)*/};
+extern const uint8_t imc_message_type_count;    // number of message types defined.
+extern const uint8_t imc_message_length[7]; 
+extern const uint8_t imc_resp_length[7];
+
 #endif
 
 
