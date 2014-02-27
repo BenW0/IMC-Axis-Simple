a64f5368ee8ca380ae8e1e9935911a8da3c706eb
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-02-26 22:36:01 -0800

    Rough implementation of settings, although getting/setting some isn't actually implemented

--- parameters.c
+++ parameters.c
@@ -8,8 +8,6 @@ void reset_parameters(void){
   parameters.error_low = 0;
   parameters.error_high = 0;
   parameters.homing = ENABLE_MIN; // Don't flip, home low, no software max, don't invert
-  parameters.pullup_low = IMC_PULLUP;
-  parameters.pullup_high = IMC_PULLUP;
   parameters.min_pos = 0;
   parameters.max_pos = 0;
 
@@ -40,8 +38,9 @@ uint32_t const_to_mask(imc_axis_parameter c){
     return INVERT_MIN;
   case IMC_PARAM_MAX_LIMIT_INV:
     return INVERT_MAX;
+  default:
+    return 0;
   }
-  return 0;
 }
 
 void handle_get_parameter(msg_get_param_t* msg,rsp_get_param_t* rsp){
@@ -62,12 +61,6 @@ void handle_get_parameter(msg_get_param_t* msg,rsp_get_param_t* rsp){
   case IMC_PARAM_MAX_LIMIT_INV:
     rsp->value = const_to_mask(msg->param_id) & parameters.homing ? 1 : 0;
     break;
-  case IMC_PARAM_MIN_LIMIT_PULLUP:
-    rsp->value = parameters.pullup_low;
-    break;
-  case IMC_PARAM_MAX_LIMIT_PULLUP:
-    rsp->value = parameters.pullup_high;
-    break;
   case IMC_PARAM_MIN_POS:
     rsp->value = parameters.min_pos;
     break;
@@ -77,23 +70,63 @@ void handle_get_parameter(msg_get_param_t* msg,rsp_get_param_t* rsp){
   case IMC_PARAM_HOME_POS:
     rsp->value = parameters.home_pos;
     break;
-   
-   
-
+  case IMC_PARAM_HOMING_FEEDRATE:
+    rsp->value = parameters.homing_feedrate;
+    break;
+  case IMC_PARAM_MOTOR_ON:
+    rsp->value = parameters.motor_on;
+    break;
+  case IMC_PARAM_MOTOR_IDLE_TIMEOUT:
+    rsp->value = parameters.motor_timeout;
+    break;
+  case IMC_PARAM_LOCATION:
+    // Consult the stepper driver state
+    ;
+    break;
+  case IMC_PARAM_SLOWDOWN:
+    rsp->value = parameters.slowdown;
+    break;
+  case IMC_PARAM_SYNC_ERROR:
+    rsp->value = parameters.sync_error;
+    break;
+  default:
+    ;
   }
 }
-/*
-  IMC_PARAM_HOME_POS,
-  IMC_PARAM_HOMING_FEEDRATE,
-  IMC_PARAM_MOTOR_ON,
-  IMC_PARAM_MOTOR_IDLE_TIMEOUT,
-  IMC_PARAM_SLOWDOWN,
-  IMC_PARAM_LOCATION,
-  IMC_PARAM_SYNC_ERROR      		// read only
-*/
-
-
-
 
 void handle_set_parameter(msg_set_param_t* msg){
+  // Can't set error info or sync_error
+  // Pullups and motor on/off are special, as we actually have to do io
+  uint32_t mask;
+  switch(msg->param_id){
+  case IMC_PARAM_FLIP_AXIS:
+  case IMC_PARAM_HOME_DIR:
+  case IMC_PARAM_MIN_SOFTWARE_ENDSTOPS:
+  case IMC_PARAM_MAX_SOFTWARE_ENDSTOPS:
+  case IMC_PARAM_MIN_LIMIT_EN:
+  case IMC_PARAM_MAX_LIMIT_EN:
+  case IMC_PARAM_MIN_LIMIT_INV:
+  case IMC_PARAM_MAX_LIMIT_INV:
+    mask = const_to_mask(msg->param_id);
+    parameters.homing = ((~mask) & parameters.homing) | (msg->param_value ? mask : 0);
+    break;
+  case IMC_PARAM_MIN_POS: parameters.min_pos = msg->param_value; break;
+  case IMC_PARAM_MAX_POS: parameters.max_pos = msg->param_value; break;
+  case IMC_PARAM_HOMING_FEEDRATE: parameters.homing_feedrate = msg->param_value; break;
+  case IMC_PARAM_HOME_POS: parameters.home_pos = msg->param_value; break;
+  case IMC_PARAM_MOTOR_IDLE_TIMEOUT: parameters.motor_timeout = msg->param_value; break;
+  case IMC_PARAM_SLOWDOWN: parameters.slowdown = msg->param_value; break;
+  case IMC_PARAM_MIN_LIMIT_PULLUP:
+    // Do some IO to set the pullup value directly
+    break;
+  case IMC_PARAM_MAX_LIMIT_PULLUP:
+    // Do some IO to set the pullup value directly
+    break;
+  case IMC_PARAM_MOTOR_ON:
+    // Do some IO to turn the motor on or off
+    parameters.motor_on = msg->param_value; 
+    break;
+  default:
+    break;
+  }
 }
---
+++ parameters.h
@@ -0,0 +1,38 @@
+#ifndef parameter_h
+#define parameter_h
+
+#include "protocol/constants.h"
+#include "protocol/message_structs.h"
+#include <stdint.h>
+
+#define FLIP_AXIS (1<<0)
+#define HOME_DIR  (1<<1)
+#define MIN_SOFTWARE (1<<2)
+#define MAX_SOFTWARE (1<<3)
+#define ENABLE_MIN (1<<4)
+#define ENABLE_MAX (1<<5)
+#define INVERT_MIN (1<<6)
+#define INVERT_MAX (1<<7)
+
+typedef struct {
+  uint32_t error_low;
+  uint32_t error_high;
+  uint32_t homing; // Bit mask with fields defined by the constants at top of file
+  uint32_t min_pos;
+  uint32_t max_pos;
+  uint32_t home_pos;
+  uint32_t homing_feedrate;
+  uint32_t motor_on;
+  uint32_t motor_timeout;
+  // Position lives in the stepper state
+  uint32_t slowdown;
+  uint32_t sync_error;
+} parameters_t;
+
+extern parameters_t parameters;
+
+void reset_parameters(void);
+void handle_get_parameter(msg_get_param_t*,rsp_get_param_t*);
+void handle_set_parameter(msg_set_param_t*);
+
+#endif
