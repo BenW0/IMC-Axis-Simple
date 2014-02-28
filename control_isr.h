7d89935a369d662bf49683a0c0ea05545f2d3b36
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2014-02-27 20:51:02 -0800

    General cleanup

--- control_isr.h
+++ control_isr.h
@@ -1,9 +1,9 @@
-#ifndef sync_h
-#define sync_h
+#ifndef control_isr_h
+#define control_isr_h
 
 #define SYNC_TIMEOUT 4800000 // sync timeout defaults to 100 ms
 #define SYNC_DELAY 48 // One MS
 
-void enable_sync_isr(void);
+void enable_sync_interrupt(void); 
 
 #endif
